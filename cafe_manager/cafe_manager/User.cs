73428fe9a8805cd6e9ba27b2b828bacfe91be13a
suyebsiddiqui <suyeb30391@gmail.com>
2017-04-07 11:46:26 +0200

    added method for the registration and login for the user

--- cafe_manager/cafe_manager/User.cs
+++ cafe_manager/cafe_manager/User.cs
@@ -17,5 +17,54 @@ namespace cafe_manager
             return false;
 
         }
-    }
+        
+        //To get the user Details after Login
+        User getUserDetails(User user)
+        {
+            user=dbconnector.getUserDetails(user);
+            //check if user is null
+            return user;
+        }
+     
+        // To Authnenticate user
+        
+        bool authenticateUser(String username, String password)
+        {
+            if (dbconnector.authenticateUser(username,password))
+                Return true;
+            else
+                return false;
+        }
+                
+        //To check if the username is unique or not
+        bool checkUsername(String username)
+        {
+            if(dbconnector.checkUsername(username))
+                Return true;
+            else
+                return false;
+        }
+        
+        //To check if the mobile is unique or not
+        bool checkMobile(int mobile)
+        {
+            if(dbconnector.checkMobile(username))
+                Return true;
+            else
+                return false;
+        }
+        
+         //To check if the Email is unique or not
+        bool checkEmail(String email)
+        {
+            if(dbconnector.checkEmail(username))
+                Return true;
+            else
+                return false;
+        }
+                    
+   
+    
+    
+    }   
 }
