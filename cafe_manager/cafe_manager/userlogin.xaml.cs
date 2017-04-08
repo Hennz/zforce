40a25d7d51a696082937153793f0ff2592ad36f6
suyebsiddiqui <zforce>
2017-04-08 17:41:52 +0530

    new changes

--- cafe_manager/cafe_manager/userlogin.xaml.cs
+++ cafe_manager/cafe_manager/userlogin.xaml.cs
@@ -20,6 +20,7 @@ namespace cafe_manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        User user= new User();
         public MainWindow()
         {
             InitializeComponent();
@@ -28,8 +29,20 @@ namespace cafe_manager
         // To authenticate the user
         private void btn_signin_Click(object sender, RoutedEventArgs e)
         {
-         //call the authentication method of User class
-         // If success then all the getUserDetails method to get the user details
+            //call the authentication method of User class
+            // If success then all the getUserDetails method to get the user details
+            string username = text_username1.Text.ToString();
+            string password = text_password1.Text.ToString();
+            if (user.authenticateUser(username, password))
+            {
+                user = user.getUserDetailsByName(user);
+                //open the welcome page
+            }
+
+            else
+                //print error message
+                ;
+
         }
         
         // To open the Registration window
@@ -39,10 +52,9 @@ namespace cafe_manager
         }
         
         // To open the ForgotPassword Window window
-        private void btn_signup_Click(object sender, RoutedEventArgs e)
+        private void btn_forgotpassword_Click(object sender, RoutedEventArgs e)
         {
-         //open the Forgot password window
+            //open the Forgot password window
         }
-        
     }
 }
