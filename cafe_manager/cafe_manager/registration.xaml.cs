c39ca6e44b79692cb0bdc3c8b916dd5822d16ed1
fod12 <kewalkrishna12@gmail.com>
2017-04-08 17:22:12 +0530

    Login & registration page updated

--- cafe_manager/cafe_manager/registration.xaml.cs
+++ cafe_manager/cafe_manager/registration.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,8 +29,38 @@ namespace cafe_manager
 
         private void btn_register_Click(object sender, RoutedEventArgs e)
         {
-         //Check if all the details filled are valid
-         //call the userRegisration method to register the user
+            //Check if all the details filled are valid
+            //call the userRegisration method to register the user
+            string username = txt_username.Text.ToString();
+            string password = txt_password.Password.ToString();
+            string name = txt_name.Text.ToString();
+            string email = txt_email.Text.ToString();
+            string mobile = txt_mobile.Text.ToString();
+            string birthdate = dp_birthdate.Text.ToString();
+            string city = txt_city.Text.ToString();
+            string state = txt_state.Text.ToString();
+            string country = txt_country.Text.ToString();
+            string pin = txt_pin.Text.ToString();
+            string gender;
+                if (gender_male.IsEnabled)
+            { gender = "Male";
+            }
+                if (gender_female.IsEnabled)
+            { gender = "Female";
+            }
+
+            if (username == "" || password == "" || name == "" || email == "" || mobile == "" || city == "" || state == "" || pin == "" || country == ""|| (gender_male.IsEnabled==false && gender_female.IsEnabled== false) )
+            {
+                MessageBox.Show("Please Enter details in all the *(Mandatory) fields");
+                return;
+            }
+            if (Regex.IsMatch(username, @"^[a-zA-Z0-9_]{5,20}$") == false)
+            {
+                MessageBox.Show("Enter correct Username");
+                return;
+            }
+
+
         }
     }
 }
